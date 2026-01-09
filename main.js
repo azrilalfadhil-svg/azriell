307d7a068da6552a6501b34a4895269d8db08ee1
azrilalfadhil-svg <azrilalfadhil@gmail.com>
2026-01-09 20:36:08 +0800

    setup project

---
+++ main.js
@@ -0,0 +1,15 @@
+let nama = "azriel"; // tipe string
+let usia = 20; // tipe number
+let tinggibadan = 174.5; //tipe double float
+let pacar = 1;
+
+if (pacar == null) {
+  pacar = "belum Ada";
+} else {
+  pacar = "sudah punyaa";
+}
+alert(
+  `halo nama saya ${nama}, usia saya ${usia} tahun, tinggi badan saya ${tinggibadan} cm, status pacar saya ${pacar}`
+);
+
+globalThis.alert("selesai");
---
+++ saya.js
@@ -0,0 +1,26 @@
+let azriel= "hello dunia"
+console.log(azriel)
+let azriel2 = "HELLO WORD"
+const semester = 7;
+console.log(azriel2)
+let saya = "apa kabar"
+const kapan = "gasss"
+console.log(saya)
+console.log(kapan)
+let aku = "GANTENG"
+console.log(aku)
+let nama = 'kapan';
+console.log(`halo nama saya ${nama}, salam kenal!`)
+let kamu = "ganteng"
+let dia = "bucin"
+console.log(`kamu itu ${kamu}, dia itu ${dia}`)
+let umur =30 
+
+console.log(`umur saya adalah ${umur} tahun`)
+let tinggi = 175
+console.log(`tinggi badan saya adalah ${tinggi} cm`)
+let berat = 70
+console.log(`berat badan saya adalah ${berat} kg`)
+
+console.log(`umur saya adalah ${umur} tahun`)
+
